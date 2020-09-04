d22f1c8764f4b37d198d8d2ddf1cf2072ca5aaf0
Damian de Vega <damiandevega@gmail.com>
2020-09-04 16:02:46 -0400

    adding form and input JSX to pass red tests

--- src/Input.js
+++ src/Input.js
@@ -3,16 +3,34 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
   render() {
+    const contents = this.props.success ? null : (
+      <form className="form-inline">
+        <input 
+          type="text" 
+          className="mb-2 mx-sm-3" 
+          placeholder="enter guess"
+          data-test="input-box" 
+          />
+          <button 
+            type="submit" 
+            className="btn btn-primary mb-2"
+            data-test="submit-button"
+          >
+            Submit
+          </button>
+      </form>
+    );
+
     return (
-      <div>
-        <button></button>
+      <div data-test="component-input">
+        {contents}
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 }
 
 export default connect(mapStateToProps)(Input);
