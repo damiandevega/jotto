0caacde9e5f0798793a88f0a33693f2dfcd4f71d
Damian de Vega <damiandevega@gmail.com>
2020-09-08 13:03:38 -0400

    setting up first failing integration test

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,9 +2,28 @@ import { storeFactory } from '../test/testUtils';
 import { guessWord } from './actions';
 
 describe('guessWord action dispatcher', () => {
-  describe('no guessed words', () => {
-    test('updates state correctly for unsuccessful guess', () => {
+  const secretWord = 'party';
+  const unsuccessfulGuessedWord = 'train';
 
+  describe('no guessed words', () => {
+    let store;
+    const initialState = { secretWord };
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    })
+
+    test('updates state correctly for unsuccessful guess', () => {
+      store.dispatch(guessWord(unsuccessfulGuessedWord));
+      const newState = store.getState();
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [{
+          guessedWord: unsuccessfulGuessedWord,
+          letterMatchCount: 3
+        }]
+      }
+      expect(newState).toEqual(expectedState);
     });
 
     test('updates state correctly for successful guess', () => {
