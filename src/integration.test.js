c1d3599171077c5e4b8b3bc4a03e68c98887422f
Damian de Vega <damiandevega@gmail.com>
2020-09-08 13:46:15 -0400

    finishing failing tests for red green testing

--- src/integration.test.js
+++ src/integration.test.js
@@ -42,12 +42,33 @@ describe('guessWord action dispatcher', () => {
   });
 
   describe('some guessed words', () => {
-    test('updates state correctly for unsuccessful guess', () => {
+    const guessedWords = [{ guessedWord: 'agile', letterMatchCount: 1 }];
+    const initialState = { guessedWords, secretWord };
+    let store;
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    })
 
+    test('updates state correctly for unsuccessful guess', () => {
+      store.dispatch(guessWord(unsuccessfulGuessedWord));
+      const newState = store.getState();
+      const expectedState = {
+        secretWord,
+        success: false,
+        guessedWords: [...guessedWords, { guessedWord: unsuccessfulGuessedWord, letterMatchCount: 3 }]
+      }
+      expect(newState).toEqual(expectedState);
     });
 
     test('updates state correctly for successful guess', () => {
-
+      store.dispatch(guessWord(secretWord));
+      const newState = store.getState();
+      const expectedState = {
+        secretWord,
+        success: true,
+        guessedWords: [...guessedWords, { guessedWord: secretWord, letterMatchCount: 5 }]
+      }
+      expect(newState).toEqual(expectedState);
     });
   });
 });
