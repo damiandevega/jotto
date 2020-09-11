76aafc1ade2d06bd903cd0f109af49e902f42bc9
Damian de Vega <damiandevega@gmail.com>
2020-09-11 12:07:31 -0400

    fixing test errors by implementing connect in App component

--- src/App.test.js
+++ src/App.test.js
@@ -24,4 +24,17 @@ describe('redux properties', () => {
     const secretWordProp = wrapper.instance().props.secretWord;
     expect(secretWordProp).toBe(secretWord);
   });
+
+  test('has access to `guessedWords` state', () => {
+    const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toEqual(guessedWords);
+  });
+
+  test('`getSecretWord` action creator is a function on the props', () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
 });
