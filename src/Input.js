33ceb07c271ebcea770dc6e18e647b492d70c3b8
Damian de Vega <damiandevega@gmail.com>
2020-09-15 12:11:44 -0400

    testing input action creator click

--- src/Input.js
+++ src/Input.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import { guessWord } from './actions';
 
-class Input extends Component {
+export class UnconnectedInput extends Component {
   render() {
     const contents = this.props.success ? null : (
       <form className="form-inline">
@@ -35,4 +35,4 @@ const mapStateToProps = ({ success }) => {
   return { success };
 }
 
-export default connect(mapStateToProps, { guessWord })(Input);
+export default connect(mapStateToProps, { guessWord })(UnconnectedInput);
--- src/Input.test.js
+++ src/Input.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { findByTestAttr, storeFactory } from '../test/testUtils';
-import Input from './Input';
+import Input, { UnconnectedInput } from './Input';
 import { guessWord } from './actions';
 
 const setup = (initialState = {}) => {
@@ -17,47 +17,47 @@ describe('render', () => {
     beforeEach(() => {
       const initialState = { success: false };
       wrapper = setup(initialState);
-    })
+    });
 
     test('renders component without error', () => {
       const component = findByTestAttr(wrapper, 'component-input');
       expect(component.length).toBe(1);
-    })
+    });
 
     test('renders input box', () => {
       const inputBox = findByTestAttr(wrapper, 'input-box');
       expect(inputBox.length).toBe(1);
-    })
+    });
 
     test('renders submit button', () => {
       const submitButton = findByTestAttr(wrapper, 'submit-button');
       expect(submitButton.length).toBe(1);
-    })
-  })
+    });
+  });
 
   describe('word has been guessed', () => {
     let wrapper;
     beforeEach(() => {
       const initialState = { success: true };
       wrapper = setup(initialState);
-    })
+    });
 
     test('renders component without error', () => {
       const component = findByTestAttr(wrapper, 'component-input');
       expect(component.length).toBe(1);
-    })
+    });
 
     test('does not render input box', () => {
       const inputBox = findByTestAttr(wrapper, 'input-box');
       expect(inputBox.length).toBe(0);
-    })
+    });
 
     test('does not render submit button', () => {
       const submitButton = findByTestAttr(wrapper, 'submit-button');
       expect(submitButton.length).toBe(0);
-    })
-  })
-})
+    });
+  });
+});
 
 describe('redux props', () => {
   test('has success piece of state as prop', () => {
@@ -65,11 +65,33 @@ describe('redux props', () => {
     const wrapper = setup({ success });
     const successProp = wrapper.instance().props.success;
     expect(successProp).toBe(success);
-  })
+  });
 
   test('`guessWord` action creator is a function prop', () => {
     const wrapper = setup();
     const guessWordProp = wrapper.instance().props.guessWord;
     expect(guessWordProp).toBeInstanceOf(Function);
-  })
-})
+  });
+});
+
+describe('`guessWord` action creator call', () => {
+  test('calls `guessWord` when button is clicked', () => {
+    const guessWordMock = jest.fn();
+
+    const props = {
+      guessWord: guessWordMock
+    };
+
+    // set up app component with guessWordMock as the guessWord prop
+    const wrapper = shallow(<UnconnectedInput {...props} />);
+
+    // simulate click
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    submitButton.simulate('click');
+
+    // check to see if mock ran
+    const guessWordCallCount = guessWordMock.mock.calls.length;
+    
+    expect(guessWordCallCount).toBe(1);
+  });
+});
